795c9806639196893b648de547d97fcf7b3c95ab
Imma <imma.becker@outlook.de>
2020-03-20 20:17:27 +0100

    added wobbly leg

---
+++ Assets/Scripts/Rand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public static class Rand
+{
+    private static System.Random generator = new System.Random();
+
+    public static bool Bool(float prob) { return generator.NextDouble() < prob; }
+
+    public static int Int() { return generator.Next(); }
+    public static int Int(int max) { return generator.Next(max); }
+    public static int Int(int min, int max) { return generator.Next(min, max); }
+
+    public static float Float() { return (float)generator.NextDouble(); }
+    public static float Float(float max) { return (float)generator.NextDouble() * max; }
+    public static float Float(float min, float max) { return (float)generator.NextDouble() * (max - min) + min; }
+}
---
+++ Assets/Scripts/Wagon.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wagon : MonoBehaviour
+{
+    public Vector2 maximumShake;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    internal void Shake()
+    {
+        Vector2 randomShake = maximumShake * new Vector2(Rand.Float(-1, 1), Rand.Float(-1, 1));
+    }
+}
