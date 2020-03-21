64a671ec4bad39feeae19e3edbb61e1754ab7dc1
Imma <imma.becker@outlook.de>
2020-03-21 14:30:46 +0100

    Camera following player

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform followTarget;
+    [SerializeField][Range(0, 1)] private float followSpeed;
+
+    private void FixedUpdate()
+    {
+        float xDiff = followTarget.position.x - transform.position.x;
+        transform.position = new Vector3(transform.position.x + xDiff * followSpeed * Time.deltaTime, transform.position.y, transform.position.z);
+    }
+}
